101dd42f951d7b2f2fd63692ab07ab22336e2eda
Jose Trinidad Fajardo Flores <trinof@192.168.248.128>
2017-10-29 14:09:15 -0700

    Initial Commit

---
+++ MiPrimeraAnalytics/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  MiPrimeraAnalytics
+//
+//  Created by Jose Trinidad Fajardo Flores on 10/29/17.
+//  Copyright © 2017 Jose Trinidad Fajardo Flores. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ MiPrimeraAnalytics/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MiPrimeraAnalytics
+//
+//  Created by Jose Trinidad Fajardo Flores on 10/29/17.
+//  Copyright © 2017 Jose Trinidad Fajardo Flores. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
