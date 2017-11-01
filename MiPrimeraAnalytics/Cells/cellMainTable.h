fe5a3eba7b716d537693e1db0568a598fc133905
Jose Trinidad Fajardo Flores <trinof@192.168.248.128>
2017-10-31 23:09:35 -0700

    Added cell objects

---
+++ MiPrimeraAnalytics/Cells/cellMainTable.h
@@ -0,0 +1,15 @@
+//
+//  cellMainTable.h
+//  MiPrimeraAnalytics
+//
+//  Created by Jose Trinidad Fajardo Flores on 10/31/17.
+//  Copyright © 2017 Jose Trinidad Fajardo Flores. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cellMainTable : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *imgItem;
+@property (weak, nonatomic) IBOutlet UILabel *lblName;
+
+@end
---
+++ MiPrimeraAnalytics/Cells/cellMainTable.m
@@ -0,0 +1,24 @@
+//
+//  cellMainTable.m
+//  MiPrimeraAnalytics
+//
+//  Created by Jose Trinidad Fajardo Flores on 10/31/17.
+//  Copyright © 2017 Jose Trinidad Fajardo Flores. All rights reserved.
+//
+
+#import "cellMainTable.h"
+
+@implementation cellMainTable
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
