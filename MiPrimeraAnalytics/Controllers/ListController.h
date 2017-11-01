1e7b956a49b46b38031ba866dad657e4d6db857e
Jose Trinidad Fajardo Flores <trinof@192.168.248.128>
2017-10-31 23:33:18 -0700

    Added table rows

--- MiPrimeraAnalytics/Controllers/ListController.h
+++ MiPrimeraAnalytics/Controllers/ListController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface ListController : UIViewController
+@property (weak, nonatomic) IBOutlet UITableView *tblMain;
 
 @end
--- MiPrimeraAnalytics/Controllers/ListController.m
+++ MiPrimeraAnalytics/Controllers/ListController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ListController.h"
+#import "cellMainTable.h"
 
 @interface ListController ()
+
+@property NSMutableArray *itemNames;
+
+@property NSMutableArray *itemImages;
 
 @end
 
@@ -16,12 +21,54 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self initController];
     // Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+//-------------------------------------------------------------------------------
+- (void)initController {
+    self.itemNames  = [[NSMutableArray alloc] initWithObjects: @"Zapatos", @"Chamarra", @"Playstation 4", @"Mochila deportiva", nil];
+    
+    self.itemImages = [[NSMutableArray alloc] initWithObjects: @"zapatos.jpg", @"chamarra.jpg", @"ps4.jpg", @"mochila.jpg", nil];
+}
+
+/**********************************************************************************************/
+#pragma mark - Table source and delegate methods
+/**********************************************************************************************/
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+//-------------------------------------------------------------------------------
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.itemNames.count;
+}
+//-------------------------------------------------------------------------------
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 64;
+}
+//-------------------------------------------------------------------------------
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    //Initialize cells
+    cellMainTable *cell = (cellMainTable *)[tableView dequeueReusableCellWithIdentifier:@"cellMainTable"];
+    
+    if (cell == nil) {
+        [tableView registerNib:[UINib nibWithNibName:@"cellMainTable" bundle:nil] forCellReuseIdentifier:@"cellMainTable"];
+        cell = [tableView dequeueReusableCellWithIdentifier:@"cellMainTable"];
+    }
+    //Fill cell with info from arrays
+    cell.lblName.text       = self.itemNames[indexPath.row];
+    cell.imgItem.image      = [UIImage imageNamed:self.itemImages[indexPath.row]];
+    
+    return cell;
+}
+//-------------------------------------------------------------------------------
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    //Pending
 }
 
 /*
